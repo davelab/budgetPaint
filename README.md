4f233cf398e149303962bc3adc8a5de382381734
Davide Cariani <davidecariani7@gmail.com>
2015-02-16 20:07:46 +0100

    update readme and add comments on painter.js

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# BudgetPaint	
+# BudgetPaint
 
-With this app you can draw on a full size canvas html canvas. You can choose every colours you love, the stroke width from a draggable tools palette, which allow you to draw on every single pixel of the canvas.
+With this app you can draw on a full size HTML canvas. You can choose every colours you love, the stroke width from a draggable tools palette, which allow you to draw on every single pixel of your canvas.
+
+features:
+ - Floating draggable pallette tools (jquery UI)
+ - Touch ready
+ - Color picker (minicolors jquery plugin)
+ - Stroke width slider tool (noUIslider jquery plugin)
+ - undo action
+ - redo action
+ - clear canvas action
 
 ![screen](/app/images/screenshot.png)
 
@@ -13,13 +22,13 @@ for run this project type on your terminal:
 
 <pre>
 > npm install && bower install
-> grunt dev 
+> grunt dev
 </pre>
 
 then open your browser at http://localhost:8081/
 
 ## @todo
 
-- create grunt task for that manage bower components and concact, minify javascripts for distribution purpose
+- create grunt task for that manage bower components and concact, minify javascripts for deploy purpose
 
 - create qUnit Test
--- app/scripts/painter.js
+++ app/scripts/painter.js
@@ -1,4 +1,5 @@
 var Painter = (function($){
+  //create all ui and canvas default settings
   var canvas          = $('#paint')[0],
       ctx             = canvas.getContext('2d'),
       sketch          = $('.canvas-container')[0],
@@ -27,19 +28,21 @@ var Painter = (function($){
         lineCap     : 'round',
         strokeStyle : '#f26522'
       };
-
   var Init = function() {
     setCanvas();
   };
 
+  //attach draggable functionality to palette tools
   ui.toolsPalette.draggable({
     handle: ui.paletteHandle,
     containment: ui.canvas,
     scroll: false
   });
 
+  //set default color on the palette
   ui.colorPalette.css({'background-color' : defaultStyle.strokeStyle});
 
+  //attach colopicker functionality on color tool palette
   ui.colorpicker.minicolors({
     inline: true,
     defaultValue: defaultStyle.strokeStyle,
@@ -51,6 +54,7 @@ var Painter = (function($){
     }
   });
 
+  //attach colopicker functionality on stroke tool palette
   ui.slider.noUiSlider({
       start: [ defaultStyle.strokeWidth ],
       step: 1,
@@ -66,24 +70,28 @@ var Painter = (function($){
       }
   }).Link('lower').to(ui.sliderStrokeVal, null, wNumb({decimals: 0}));
 
+  //bind clear canvas event and reset mem
   ui.clearPalette.on('click', function(e) {
     totalReset();
   });
+  //bind undo action
   ui.undoPalette.on('click', function(e) {
     undo();
   });
+  //bind redo action
   ui.redoPalette.on('click', function(e) {
     redo();
   });
-
+  //bind show and hide event for stroke slider
   ui.strokePalette.on('click', function(e) {
     showControl(ui.sliderContainer, e);
   });
-
+  //bind show and hide event for color picker
   ui.colorPalette.on('click', function(e) {
     showControl(ui.colorpickerContainer, e);
   });
 
+  //create object manager for touch events for drawing on canvas
   var onPaintTouch = {
      isDrawing: false,
      touchstart: function(coors){
@@ -121,7 +129,7 @@ var Painter = (function($){
   canvas.addEventListener('touchmove', draw, false);
   canvas.addEventListener('touchend', draw, false);
 
-
+  // attach the mousemove listner
   canvas.addEventListener('mousemove', function(e) {
     last_mouse.x = mouse.x;
     last_mouse.y = mouse.y;
@@ -131,16 +139,19 @@ var Painter = (function($){
 
   }, false);
 
+  // attach the mousedown listner and call onPaint function
   canvas.addEventListener('mousedown', function(e) {
       canvas.addEventListener('mousemove', onPaint, false);
       onPaint();
   }, false);
 
+  // attach the mouseup listner and create a draw's snapshot
   canvas.addEventListener('mouseup', function(e) {
       canvas.removeEventListener('mousemove', onPaint, false);
       createSnapshot();
   }, false);
 
+  //utility function for get viewport properties
   var _getViewport = function () {
     var m = document.compatMode == 'CSS1Compat';
     return {
@@ -149,8 +160,9 @@ var Painter = (function($){
       w : window.innerWidth || (m ? document.documentElement.clientWidth : document.body.clientWidth),
       h : window.innerHeight || (m ? document.documentElement.clientHeight : document.body.clientHeight)
     };
-  }
+  };
 
+  //define basic function for canvas draw actions on mouse events
   var onPaint = function() {
       ctx.beginPath();
       ctx.moveTo(last_mouse.x, last_mouse.y);
@@ -159,6 +171,7 @@ var Painter = (function($){
       ctx.stroke();
   };
 
+  //set up default valuse for canvas
   var setCanvas = function() {
     canvas.width      = parseInt(sketchStyle.getPropertyValue('width'));
     canvas.height     = parseInt(sketchStyle.getPropertyValue('height'));
@@ -168,22 +181,26 @@ var Painter = (function($){
     ctx.strokeStyle   = defaultStyle.strokeStyle;
   };
 
+  //define clear canvas action
   var clearCanvas = function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   };
 
+  //define complete clear canvas and reset the draw's snapshots
   var totalReset = function() {
     clearCanvas();
     snapshots = [];
     segment = -1;
   };
 
+  //define a function for create snapshot of current draw
   var createSnapshot = function() {
     segment++;
     if (segment < snapshots.length) { snapshots.length = segment }
     snapshots.push(canvas.toDataURL());
   };
 
+  //define restore previous snapshot function
   var undo = function() {
      if (segment > 0) {
         segment--;
@@ -193,7 +210,7 @@ var Painter = (function($){
         ctx.drawImage(snapshot, 0, 0);
       }
   };
-
+  //define redo function
   var redo = function() {
     if (segment < snapshots.length-1) {
         segment++;
@@ -202,7 +219,8 @@ var Painter = (function($){
         snapshot.onload = function () { ctx.drawImage(snapshot, 0, 0); }
     }
   };
-
+  //define the show control function that allow to set up where to show controls
+  //based on the tools palette current position
   var showControl = function(el, ev) {
     if (!el.hasClass('active')) {
       var viewPort      = _getViewport(),
